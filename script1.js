4fddf5a054ae1f54e13809e247544e02f5d09cd5
Tim <tim.duke.314@gmail.com>
2025-05-29 11:30:24 -0500

    Added button flash event template.

--- script1.js
+++ script1.js
@@ -1,4 +1,10 @@
-// Calculator Functions
+// Calculation Variables
+let number1 = '';
+let operator = '';
+let number2 = '';
+updateScreen(number1, operator, number2);
+
+// Operator Functions
 function add (num1, num2) {
   return (num1 + num2);
 };
@@ -15,7 +21,19 @@ function divide (num1, num2) {
   return (num1 / num2);
 };
 
-// Calculation Variables
-let number1;
-let operator;
-let number2;
+// Display Functions
+function updateScreen (num1, oper, num2) {
+  document.querySelector('.screen').textContent = `${num1} ${oper} ${num2}`;
+};
+
+// Button Event Functions
+let buttonOne = document.querySelector('.button');
+buttonOne.addEventListener("mousedown", buttonDown);
+buttonOne.addEventListener("mouseup", buttonUp);
+
+function buttonDown (event) {
+  event.target.style.backgroundColor = 'red';
+};
+function buttonUp (event) {
+  event.target.style.backgroundColor = 'rgb(100 100 100)';
+};
